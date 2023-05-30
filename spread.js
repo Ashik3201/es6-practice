0b657a3b233cd19dd2dc590a848111efe376c181
Abdullah Al Ashik01~ <abdullah.al.ashik1@gmail.com>
2023-05-31 00:25:31 +0600

    conti,,

---
+++ spread.js
@@ -0,0 +1,15 @@
+const ages = [12,34,56,78,55];
+const ages2 = [32,45,67,68,9];
+const allAges = [...ages,...ages2,...[43]];
+console.log(allAges);
+
+const minister =4545;
+const sochib = 4343;
+const business = 232323;
+const maximum  = Math.max(minister,sochib,business);
+console.log(maximum);
+
+
+const borolok = [3434,54454,5656,65656];
+const max = Math.max(...borolok);
+console.log(max);
